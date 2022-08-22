c22ec922e567e1bd6407504e5aaa46e49b8c93c7
Zelt <392321069@qq.com>
2022-08-22 13:15:27 +0000

    ch1 main content finished; add print!

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,14 +1,22 @@
 #![no_main]
 #![no_std]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod sbi;
 mod lang_items;
 
-use core::arch::global_asm;
-global_asm!(include_str!("entry.asm"));
+
+
+core::arch::global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    loop {}
+    // loop {}
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
 }
 
 fn clear_bss() {
