d94d5942cfda3c203df56343317798dad5e5fcc0
Zelt <392321069@qq.com>
2022-08-23 02:05:57 +0000

    add logger

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,11 +2,15 @@
 #![no_std]
 #![feature(panic_info_message)]
 
+use log::*; 
+// [TIPS] need to add dependancy log = "0.4" in Cargo.toml
+
 #[macro_use]
 mod console;
+// [TIPS] macro_use must be placed just upon the mod providing macro_export
 mod sbi;
 mod lang_items;
-
+mod logging;
 
 
 core::arch::global_asm!(include_str!("entry.asm"));
@@ -14,8 +18,28 @@ core::arch::global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    // loop {}
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn boot_stack();
+        fn boot_stack_top();
+        fn sbss();
+        fn ebss();
+    }
+    logging::init();
     println!("Hello, world!");
+    trace!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    warn!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    error!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     panic!("Shutdown machine!");
 }
 
