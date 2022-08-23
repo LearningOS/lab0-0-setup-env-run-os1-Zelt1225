96ba15f89c8da191ac187a070652f65492df72e5
Zelt <392321069@qq.com>
2022-08-23 07:43:00 +0000

    user_app barely runs

--- user/src/lang_items.rs
+++ user/src/lang_items.rs
@@ -14,4 +14,21 @@ fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
         println!("Panicked: {}", err);
     }
     exit(-1);
+    loop{}
 }
+
+// use core::panic::PanicInfo;
+
+// #[panic_handler]
+// fn panic(info: &PanicInfo) {
+//     if let Some(location) = info.location() {
+//         println!(
+//             "Panicked at {}:{} {}", 
+//             location.file(), 
+//             location.line(), 
+//             info.message().unwrap()
+//         );
+//     } else {
+//         println!("Panicked: {}", info.message().unwrap());   
+//     }
+// }
